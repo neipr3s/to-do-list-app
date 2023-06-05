1f607118955ded14e6458bdeefc964d703a9724c
neipr3s <129725761+neipr3s@users.noreply.github.com>
2023-06-05 20:44:18 +0200

    Added jQuery for adding a new item to the list

--- scripts.js
+++ scripts.js
@@ -1,29 +1,14 @@
+function newItem() {
+    
+    //Adding a new item to the list:
 
-function newItem(){
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    li.append(inputValue);
-
-    if(inputValue === '') {
-        alert('You must write something!');
-        } else {
-            $('#list').append(li);
-        }
-
-    function crossOut() {
-        li.toggleClass('.strike');
-    }
-
-    li.on('dblclick', crossOut);
-
-    let crossOutButton = $('<crossOutButton>X</crossOutButton>')
-    li.append(crossOutButton);
-
-    crossOutButton.on('click', deleteListItem);
-    function deleteListItem() {
-        li.addClass('delete');
-    }
-
-    $('#list').sortable();
-}; 
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+}
